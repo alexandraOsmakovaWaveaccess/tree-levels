3e98fef98ce4ba0ede4a012b93b6d60d6c86f61e
Alexandra Osmakova <alexandra.osmakova@gmail.com>
2019-06-18 11:53:12 +0300

    app-nav component is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavComponent
   ],
   imports: [
     BrowserModule,
